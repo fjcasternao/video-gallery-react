b3d4a13fdb4b838748d55ae063ca9e9db1dd6cec
Fran Casternao <fjcasternao@gmail.com>
2018-09-29 13:25:37 +0200

    Setup testing and adding test for router

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import HomePage from '../HomePage/HomePage';
+import DetailsPage from '../DetailsPage/DetailsPage';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(
+    <Router>
+      <App />
+    </Router>
+  );
+});
+
+it('invalid path goes to 404', () => {
+  const wrapper = mount(
+    <Router initialEntries={[ '/random' ]}>
+      <App />
+    </Router>
+  );
+  expect(wrapper.find(HomePage)).toHaveLength(0);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(1);
+});
+
+it('base path navigates home', () => {
+  const wrapper = mount(
+    <Router initialEntries={[ '/' ]}>
+      <App />
+    </Router>
+  );
+  expect(wrapper.find(HomePage)).toHaveLength(1);
+});
+
+it('movie path wit id navigates to movie details', () => {
+  const wrapper = mount(
+    <Router initialEntries={[ '/movie/123' ]}>
+      <App />
+    </Router>
+  );
+  expect(wrapper.find(DetailsPage)).toHaveLength(1);
 });
