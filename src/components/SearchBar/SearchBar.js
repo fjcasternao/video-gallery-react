1d427fe1c754ff6337950d8fa7e3160526230d08
Fran Casternao <fjcasternao@gmail.com>
2018-09-29 21:37:59 +0200

    Getting network logic out of search component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,31 +3,17 @@ import axios from 'axios';
 
 import './SearchBar.css';
 
-const API_KEY = 'd45b0197ddc4c01c4e294489cac57c05';
-const API_URL = 'https://api.themoviedb.org/3/search/movie';
-
 class SearchBar extends Component {
     state = {
-        query: '',
-        results: [],
+        query: ''
     };
-
-    getInfo = () => {
-        axios
-            .get(`${API_URL}?api_key=${API_KEY}&query=${this.state.query}`)
-            .then(({ data }) => {
-                this.props.updateResults(data.results)
-            })
-    }
 
     handleInputChange = () => {
         this.setState({
             query: this.search.value,
         }, () => {
-            if (this.state.query && this.state.query.length > 1) {
-                if (this.state.query.length % 2 === 0) {
-                    this.getInfo();
-                }
+            if (this.state.query && this.state.query.length > 2) {
+                this.props.onUpdateSearch(this.state.query);
             }
         })
     };
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -6,7 +6,8 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 import MovieItem from '../../components/MovieItem/MovieItem';
 
 const API_KEY = 'd45b0197ddc4c01c4e294489cac57c05';
-const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+const API_URL_POPULAR = 'https://api.themoviedb.org/3/movie/popular';
+const API_URL_SEARCH = 'https://api.themoviedb.org/3/search/movie';
 
 class HomePage extends React.PureComponent {
 
@@ -19,9 +20,17 @@ class HomePage extends React.PureComponent {
     }
 
     getPopularMovies = () => {
-        axios.get(`${API_URL}?api_key=${API_KEY}`)
+        axios.get(`${API_URL_POPULAR}?api_key=${API_KEY}`)
             .then(({ data }) => {
                 this.updateResults(data.results);
+            })
+    }
+
+    getMoviesByTitle = (movieTitle) => {
+        axios
+            .get(`${API_URL_SEARCH}?api_key=${API_KEY}&query=${movieTitle}`)
+            .then(({ data }) => {
+                this.updateResults(data.results)
             })
     }
 
@@ -31,12 +40,16 @@ class HomePage extends React.PureComponent {
         })
     }
 
+    onUpdateSearch = (movieTitle) => {
+        this.getMoviesByTitle(movieTitle);
+    }
+
 
     render() {
         return (
             <div>
                 <Header text="Technical interview: Movie Gallery" />
-                <SearchBar updateResults = {this.updateResults} />
+                <SearchBar onUpdateSearch = {this.onUpdateSearch} />
                 <ul>
                     {
                         this.state.results.map(movie => {
