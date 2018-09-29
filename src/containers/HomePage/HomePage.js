24fb85a47f76b6e4edd434d4d1054066f9c5418b
Fran Casternao <fjcasternao@gmail.com>
2018-09-29 14:12:14 +0200

    Getting list of popular movies from Movies API

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import axios from 'axios'
 
 import Header from '../../components/Header/Header';
 
+
+const API_KEY = 'd45b0197ddc4c01c4e294489cac57c05';
+const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+
 class HomePage extends React.PureComponent {
+
+    state = {
+        results: [],
+    };
+
+    componentDidMount = () => {
+        this.getPopularMovies();
+    }
+
+    getPopularMovies = () => {
+        axios.get(`${API_URL}?api_key=${API_KEY}`)
+            .then(({ data }) => {
+                this.setState({
+                    results: data.results                          
+                })
+            })
+    }
+
     render() {
         return (
             <div>
                 <Header text="Technical interview: Movie Gallery" />
+                <ul>
+                {
+                    this.state.results.map(movie => {
+                        return <li>{movie.title}</li>
+                    })
+                }
+                </ul>
             </div>
         );
     }
